732b4c1a77507d3c9964c508f5507686cba77e81
Anamika_S <anamika.sawhney22@gmail.com>
2022-03-02 09:22:04 +0000

    Add project files.

---
+++ CommonFunctions/NumericFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CommonFunctions
+{
+    public class NumericFunctions
+    {
+        public int Add(params int []num)
+        {
+            int sum = 0;
+            foreach (int temp in num)
+                sum += temp;
+            return sum;
+
+        }
+        public bool IsEven(int num)
+        {
+            return num % 2 == 0 ? true : false;
+        }
+    }
+}
---
+++ CommonFunctions/StringFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonFunctions
+{
+    public class StringFunctions
+    {
+        public string CombineNames(string s1 ,string s2)
+        {
+            return string.Concat(s1, " ", s2);
+        }
+        public string[] SplitSentence(string sentence)
+        {
+            string[] str = sentence.Split();
+            return str;
+
+        }
+        public string Table(int num, int start , int last)
+        {
+            StringBuilder sb = new StringBuilder(); 
+
+            for(int i= start; i<= last; i++)
+            {
+                string str =num + " * " + i + " = " + num * i + "\n";
+                sb.Append(str);
+            }
+            return sb.ToString(); 
+        }
+    }
+}
